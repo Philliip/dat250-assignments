235bd3f50f608c6a8a2fe3c39887a3fe337cea0e
Phillip <71596849+Philliip@users.noreply.github.com>
2024-10-06 22:38:46 +0200

    add assignment_6

---
+++ assignment_6/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "hvl.dat_250"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.rabbitmq:amqp-client:5.22.0")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
